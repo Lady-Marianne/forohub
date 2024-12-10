e6f7d33edfc2e72b38aa60857408770428003365
Lady-Marianne <mariana.andrea.lois@gmail.com>
2024-12-10 10:55:41 -0300

    Solucionado el problema de la migración conflictiva y las columnas indeseadas.

--- src/main/java/com/alura/challenge/forohub/domain/user/DataAuthenticateUser.java
+++ src/main/java/com/alura/challenge/forohub/domain/user/DataAuthenticateUser.java
@@ -1,7 +1,5 @@
 package com.alura.challenge.forohub.domain.user;
 
-import jakarta.validation.constraints.NotNull;
-
 public record DataAuthenticateUser(
         String username,
         String password
