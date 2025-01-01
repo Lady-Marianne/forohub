6b7dade2b6e37c571f9978a94f375691deafa0cf
Lady-Marianne <mariana.andrea.lois@gmail.com>
2025-01-01 13:53:15 -0300

    Mofificar UserController para que sirva tantos para usuarios comunes como para administradores.

--- src/main/java/com/alura/challenge/forohub/controllers/UserController.java
+++ src/main/java/com/alura/challenge/forohub/controllers/UserController.java
@@ -1,10 +1,8 @@
 package com.alura.challenge.forohub.controllers;
 
+import com.alura.challenge.forohub.domain.admin.Admin;
+import com.alura.challenge.forohub.domain.user.*;
 import com.alura.challenge.forohub.infra.exceptions.ValidationException;
-import com.alura.challenge.forohub.domain.user.DataRegisterUser;
-import com.alura.challenge.forohub.domain.user.DataResponseUser;
-import com.alura.challenge.forohub.domain.user.User;
-import com.alura.challenge.forohub.domain.user.UserRepository;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +16,38 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 import java.util.Optional;
 
+//@RestController
+//@RequestMapping("/users")
+//@SecurityRequirement(name = "bearer-key")
+//public class UserController {
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @PostMapping
+//    public ResponseEntity<DataResponseUser> registerUser(
+//            @RequestBody @Validated DataRegisterUser dataRegisterUser,
+//            UriComponentsBuilder uriComponentsBuilder) {
+//
+//        // Verificar si el usuario ya existe (por su correo electrónico o nombre de usuario):
+//        Optional<User> existingUser = userRepository.findByUsernameOrEmail(dataRegisterUser.username(),
+//                dataRegisterUser.email());
+//
+//        // Si ya existe, lanzar una excepción:
+//        if (existingUser.isPresent()) {
+//            throw new ValidationException("El usuario ya está registrado.");
+//        }
+//
+//        // Crear y guardar el usuario:
+//        User user = userRepository.save(new User(dataRegisterUser));
+//        // Construir URL del nuevo recurso:
+//        URI url = uriComponentsBuilder.path("/users/{userId}").buildAndExpand(user.getUserId()).toUri();
+//
+//        // Retornar respuesta utilizando el constructor del DTO:
+//        return ResponseEntity.created(url).body(new DataResponseUser(user));
+//    }
+//}
+
 @RestController
 @RequestMapping("/users")
 @SecurityRequirement(name = "bearer-key")
@@ -30,18 +60,39 @@ public class UserController {
     public ResponseEntity<DataResponseUser> registerUser(
             @RequestBody @Validated DataRegisterUser dataRegisterUser,
             UriComponentsBuilder uriComponentsBuilder) {
+        return registerUserWithRole(dataRegisterUser, Role.USER, uriComponentsBuilder);
+    }
+
+    @PostMapping("/admins")
+    public ResponseEntity<DataResponseUser> registerAdmin(
+            @RequestBody @Validated DataRegisterUser dataRegisterUser,
+            UriComponentsBuilder uriComponentsBuilder) {
+        return registerUserWithRole(dataRegisterUser, Role.ADMIN, uriComponentsBuilder);
+    }
+
+    // Método privado genérico para registrar usuarios con cualquier rol:
+    private ResponseEntity<DataResponseUser> registerUserWithRole(
+            DataRegisterUser dataRegisterUser,
+            Role role,
+            UriComponentsBuilder uriComponentsBuilder) {
 
         // Verificar si el usuario ya existe (por su correo electrónico o nombre de usuario):
         Optional<User> existingUser = userRepository.findByUsernameOrEmail(dataRegisterUser.username(),
                 dataRegisterUser.email());
 
-        // Si ya existe, lanzar una excepción:
         if (existingUser.isPresent()) {
             throw new ValidationException("El usuario ya está registrado.");
         }
 
-        // Crear y guardar el usuario:
-        User user = userRepository.save(new User(dataRegisterUser));
+        // Crear el usuario o admin con el rol especificado:
+        User user;
+        if (role == Role.ADMIN) {
+            user = new Admin(dataRegisterUser);
+        } else {
+            user = new User(dataRegisterUser);
+        }
+        userRepository.save(user);
+
         // Construir URL del nuevo recurso:
         URI url = uriComponentsBuilder.path("/users/{userId}").buildAndExpand(user.getUserId()).toUri();
 
